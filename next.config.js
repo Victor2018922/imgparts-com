f626d2589fb39bc4368ac958a27202ddd576adcc
Victor2018922 <winnie200805@gmail.com>
2025-09-08 21:40:43 +0800

    fix: disable optimizeCss in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,22 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'niuniuparts-image.oss-cn-hangzhou.aliyuncs.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'niuniuparts-image.oss-cn-hangzhou.aliyuncs.com',
-        pathname: '/**',
-      },
-    ],
-  },
-  // 保持默认：不要添加 output: 'export'
   experimental: {
-    optimizeCss: true,
+    optimizeCss: false,
   },
 };
 
