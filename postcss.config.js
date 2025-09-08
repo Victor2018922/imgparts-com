2c4c8665e9a92e2e8662eac8d7a8f63357813edb
Victor2018922 <winnie200805@gmail.com>
2025-09-08 21:03:48 +0800

    fix: correct postcss.config.js plugins config

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
-// postcss.config.js  (CommonJS)
+// postcss.config.js
 module.exports = {
   plugins: {
-    // 可留可去：如无 import 需求可删
-    'postcss-import': {},
-    // 如不需要嵌套，也可删下面一行
-    'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
   },
